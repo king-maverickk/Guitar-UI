44c2935a9b45b47f662bdb90a37f1e41244dc7f2
king-maverickk <maverick.motheo.iv@gmail.com>
2024-11-25 13:27:25 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Guitar_UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
