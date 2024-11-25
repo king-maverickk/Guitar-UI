6c9a85268378a830d4540e5128eaec13f23e7fae
king-maverickk <maverick.motheo.iv@gmail.com>
2024-11-25 17:15:27 +0200

    feat: print the key the user pressed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -10,6 +11,31 @@ namespace Guitar_UI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Print_Key_Down(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Q:
+                    MessageBox.Show("Q was pressed");
+                    break;
+                case Key.D1:
+                    MessageBox.Show("1 was pressed");
+                    break;
+                case Key.D2:
+                    MessageBox.Show("2 was pressed");
+                    break;
+                case Key.D3:
+                    MessageBox.Show("3 was pressed");
+                    break;
+                case Key.D4:
+                    MessageBox.Show("4 was pressed");
+                    break;
+                default:
+                    Console.WriteLine($"You pressed: {e.Key}");
+                    break;
+            }
         }
     }
 }
